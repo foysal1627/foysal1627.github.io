0496e47200c493fb3b5c4f1b88f48ecd891e2ebb
Foysal Mahmud <foysalmahmudanik000@gmail.com>
2025-08-06 08:42:01 +0600

    Create script.js

---
+++ math/script.js
@@ -0,0 +1,4 @@
+// This script can be used for any dynamic behavior if needed in the future.
+// For this specific problem, the solution is hardcoded in HTML.
+console.log("Webpage loaded successfully!");
+
